d76389459a846e499cd4fab64c957ba59f163413
hellowello <mayankekaghara.me007@gmail.com>
2024-04-09 00:40:57 +0530

    Resume Download

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,6 +4,33 @@ import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
+  
+   const handleDownloadPDF = () => {
+     // Path to your locally stored PDF file
+     const filePath = "Mayank_Ekaghara.pdf";
+
+     // Fetch the PDF file
+     fetch(filePath)
+       .then((response) => response.blob())
+       .then((blob) => {
+         // Create a blob URL for the PDF
+         const url = window.URL.createObjectURL(new Blob([blob]));
+
+         // Create a temporary anchor element to trigger the download
+         const link = document.createElement("a");
+         link.href = url;
+         link.setAttribute("download", "Mayank_Ekaghara.pdf");
+         document.body.appendChild(link);
+         link.click();
+         document.body.removeChild(link);
+
+         // Release the object URL
+         window.URL.revokeObjectURL(url);
+       })
+       .catch((error) => console.error("Error downloading PDF:", error));
+   };
+
+
   return (
     <section className={styles.container}>
       <div className={styles.content}>
@@ -17,6 +44,12 @@ export const Hero = () => {
           and collaboration within dynamic teams.
         </p>
         <div className={styles.buttons}>
+          <a
+            onClick={handleDownloadPDF}
+            className={styles.contactBtn}
+          >
+            Resume
+          </a>
           <a
             href="mailto:mayankekaghara.me@email.com"
             className={styles.contactBtn}
