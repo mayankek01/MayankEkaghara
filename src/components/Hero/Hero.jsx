1fccfa288c331e8456a1c91c129b136afea97cfe
hellowello <mayankekaghara.me007@gmail.com>
2024-04-09 00:49:10 +0530

    resume

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,30 +5,21 @@ import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
   
-   const handleDownloadPDF = () => {
-     // Path to your locally stored PDF file
-     const filePath = "Mayank_Ekaghara.pdf";
+    const onButtonClick = () => {
+      // using Java Script method to get PDF file
+      fetch("./Mayank_Ekaghara.pdf").then((response) => {
+        response.blob().then((blob) => {
+          // Creating new object of PDF file
+          const fileURL = window.URL.createObjectURL(blob);
 
-     // Fetch the PDF file
-     fetch(filePath)
-       .then((response) => response.blob())
-       .then((blob) => {
-         // Create a blob URL for the PDF
-         const url = window.URL.createObjectURL(new Blob([blob]));
-
-         // Create a temporary anchor element to trigger the download
-         const link = document.createElement("a");
-         link.href = url;
-         link.setAttribute("download", "Mayank_Ekaghara.pdf");
-         document.body.appendChild(link);
-         link.click();
-         document.body.removeChild(link);
-
-         // Release the object URL
-         window.URL.revokeObjectURL(url);
-       })
-       .catch((error) => console.error("Error downloading PDF:", error));
-   };
+          // Setting various property values
+          let alink = document.createElement("a");
+          alink.href = fileURL;
+          alink.download = "Mayank_Ekaghara.pdf";
+          alink.click();
+        });
+      });
+    };
 
 
   return (
@@ -44,12 +35,12 @@ export const Hero = () => {
           and collaboration within dynamic teams.
         </p>
         <div className={styles.buttons}>
-          <a
-            onClick={handleDownloadPDF}
+          {/* <a
+            onClick={onButtonClick}
             className={styles.contactBtn}
           >
             Resume
-          </a>
+          </a> */}
           <a
             href="mailto:mayankekaghara.me@email.com"
             className={styles.contactBtn}
